acfe3b8739a05b023f7a52c5fa0a052f6de0cf32
changwon <thswave@gmail.com>
2014-06-24 15:37:40 +0900

    create visitor book test class

---
+++ src/test/java/com/nhnent/basecamp/TestVisitorsBook.java
@@ -0,0 +1,14 @@
+package com.nhnent.basecamp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestVisitorsBook {
+	
+	@Test
+	public void testFail(){
+		assertFalse(false);
+	}
+
+}
