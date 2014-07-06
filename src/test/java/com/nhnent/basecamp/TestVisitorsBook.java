1f29f0224d8a8be55c7220dcf3a5333345436193
changwon <thswave@gmail.com>
2014-07-06 11:22:19 +0900

    mybatis setting

--- src/test/java/com/nhnent/basecamp/TestVisitorsBook.java
+++ src/test/java/com/nhnent/basecamp/TestVisitorsBook.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
